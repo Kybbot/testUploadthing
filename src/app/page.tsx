c1aba710a1ac56e2da3223cb6d0e12f50664cf27
Kybbot <odiykoartem@gmail.com>
2024-04-02 17:55:26 +0300

    Added UploadDropzone

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,24 @@
 "use client";
 
 import { FC } from "react";
-import { UploadButton } from "@/utils/uploadthing";
+import { UploadButton, UploadDropzone } from "@/utils/uploadthing";
 
 const Home: FC = () => {
 	return (
 		<main>
 			<UploadButton
+				endpoint="imageUploader"
+				onClientUploadComplete={(res) => {
+					// Do something with the response
+					console.log("Files: ", res);
+					alert("Upload Completed");
+				}}
+				onUploadError={(error: Error) => {
+					// Do something with the error.
+					alert(`ERROR! ${error.message}`);
+				}}
+			/>
+			<UploadDropzone
 				endpoint="imageUploader"
 				onClientUploadComplete={(res) => {
 					// Do something with the response
